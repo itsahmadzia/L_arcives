b0328b20c41f9beda2affd8b0fc269e5ed6540e4
ahmad zia <mahmad2004a8@gmail.com>
2024-04-26 17:19:07 +0500

    mongoose setup

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from "express";
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+let m= await mongoose.connect(process.env.DBURL)
+.then(()=>{
+    console.log("DB CONNECTED SUXXEXXFULLY")
+})
+.catch((err)=>{
+console.log(err)
+});
+  
+
 const app = express();
 app.listen(3001,()=>{
 console.log(`server is running at 3001`);
