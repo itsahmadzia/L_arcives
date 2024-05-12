80723963f501d3b1534e4c0edf51903a109e0c0c
ahmad zia <mahmad2004a8@gmail.com>
2024-05-12 20:28:42 +0500

    feat: Add getPostComments route and controller function

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,10 +1,11 @@
 import express from "express"
-import { createComment } from "../controllers/comment.controller.js";
+import { createComment , getPostComments} from "../controllers/comment.controller.js";
 import { verifyUser } from "../utils/verifyUser.js";
 const commentRouter = express.Router();
 
 
 // POST comments route
 commentRouter.post('/addaComment',verifyUser, createComment);
+commentRouter.get('/getpostComments/:postId', getPostComments);
 
 export {commentRouter};  
