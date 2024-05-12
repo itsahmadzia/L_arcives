72a524eec20d8efb4dfd59016ec6a2c34442a93c
ahmad zia <mahmad2004a8@gmail.com>
2024-05-12 20:20:59 +0500

    chore: Add getUserbyId route and controller function

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express"
-import {deleteUser, test,updatetheUser, signOut, getAllUsers} from "../controllers/user.controller.js"
+import {deleteUser, test,updatetheUser, signOut, getAllUsers , getUserbyId} from "../controllers/user.controller.js"
  
 import { verifyUser } from "../utils/verifyUser.js"
 
@@ -14,6 +14,7 @@ router.post("/delete/:user",verifyUser, deleteUser);
 
 
 router.post("/signout",signOut);
-router.get("/getUser",verifyUser,getAllUsers)
+router.get("/getUser",verifyUser,getAllUsers);
 
+router.post("/getUserbyId",getUserbyId);
 export default router; 
