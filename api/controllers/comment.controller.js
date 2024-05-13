51a557d34e30c6a1d8ecc59ba14e04addc2d253d
ahmad zia <mahmad2004a8@gmail.com>
2024-05-13 23:17:59 +0500

    feat: Add likeComments route and controller function

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -25,7 +25,7 @@ const getPostComments = async(req,res,next)=> {
     try {
      const comments = await Comment.find({postId:req.params.postId}).sort({createdAt: -1});
      res.status(200).json(comments); 
-     
+
     } catch (error) {
       next(error);
     }
@@ -33,4 +33,32 @@ const getPostComments = async(req,res,next)=> {
   }
 
 
-export {createComment, getPostComments}
+  const likeComments = async(req,res,next)=>{
+    try {
+        console.log(req.params.commentId)
+        console.log(req);
+        const comment = await Comment.findById(req.params.commentId)
+        if(!comment)
+        {
+            return res.status(404).json({message: "Comment not found"});
+        }
+        const userIndex = comment.likes.indexOf(req.user.id);
+        if(userIndex === -1){
+            comment.likes.push(req.user.id);    
+            comment.countofLikes+=1;
+
+        }
+        else {
+            comment.countofLikes-=1;
+
+            comment.likes.splice(userIndex, 1);
+        }
+        await comment.save();
+        res.status(200).json(comment)
+    } catch (error) {
+        next(error);
+    }
+
+  }
+
+export {createComment, getPostComments,likeComments}
--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { createComment , getPostComments} from "../controllers/comment.controller.js";
+import { createComment , getPostComments, likeComments} from "../controllers/comment.controller.js";
 import { verifyUser } from "../utils/verifyUser.js";
 const commentRouter = express.Router();
 
@@ -7,5 +7,7 @@ const commentRouter = express.Router();
 // POST comments route
 commentRouter.post('/addaComment',verifyUser, createComment);
 commentRouter.get('/getpostComments/:postId', getPostComments);
+
+commentRouter.put('/likeComment/:commentId',verifyUser,likeComments);
 
 export {commentRouter};  
