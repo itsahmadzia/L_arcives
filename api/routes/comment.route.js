e640cfe584aaaef5e6bf07b1f1851289843f599a
ahmad zia <mahmad2004a8@gmail.com>
2024-05-15 06:26:54 +0500

    feat: Add deleteComment and getcomments routes to comment controller

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { createComment , getPostComments, likeComments, editComments} from "../controllers/comment.controller.js";
+import { createComment , getPostComments, likeComments, editComments,getcomments,deleteComment} from "../controllers/comment.controller.js";
 import { verifyUser } from "../utils/verifyUser.js";
 const commentRouter = express.Router();
 
@@ -10,4 +10,7 @@ commentRouter.get('/getpostComments/:postId', getPostComments);
 
 commentRouter.put('/likeComment/:commentId',verifyUser,likeComments);
 commentRouter.put('/editComment/:commentId',verifyUser,editComments);
+
+commentRouter.delete('/deleteComment/:commentId', verifyUser, deleteComment);
+commentRouter.get('/getcomments', verifyUser, getcomments);
 export {commentRouter};  
