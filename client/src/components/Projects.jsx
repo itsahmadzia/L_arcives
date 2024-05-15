b8d01fe0e135b5ab181075bdef2c6838ade1bc96
ahmad zia <mahmad2004a8@gmail.com>
2024-05-15 06:27:36 +0500

    refactor: Update Projects component with container and content styles

--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
 
-export default function Projects() {
+const Projects = () => {
+  const containerStyle = {
+   // height: '100vh', // 100% of the viewport height
+   // overflowY: 'auto', // Enable vertical scrolling if needed
+  };
+
+  const contentStyle = {
+   // marginBottom: '1800px',
+    height: '1800px',
+  };
+
   return (
-    <div>
-      projects
+    <div style={containerStyle}>
+      <div className="container mx-auto px-4 py-8">
+        <h2 className="text-3xl font-semibold mb-4">Projects</h2>
+        <p style={contentStyle}>
+          Below are some of my top tier projects which I have done as a detective
+        </p>
+        {/* End of projects content */}
+        <p>Classified</p>
+      </div>
     </div>
-  )
-}
+  );
+};
+
+export default Projects;
